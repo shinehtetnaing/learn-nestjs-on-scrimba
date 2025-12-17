52ee435cf2576de38f821ceeeb963e2ffaa014d5
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-12-17 14:02:48 +0700

    profiles services

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -8,47 +8,61 @@ import {
   Param,
   Post,
   Put,
-  Query,
 } from '@nestjs/common';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { ProfilesService } from './profiles.service';
 
 @Controller('profiles')
 export class ProfilesController {
+  constructor(private profilesService: ProfilesService) {}
+
   // GET /profiles
+  // @Get()
+  // findAll(@Query('age') age: number) {
+  //   return [{ age }];
+  // }
   @Get()
-  findAll(@Query('age') age: number) {
-    return [{ age }];
+  findAll() {
+    return this.profilesService.findAll();
   }
 
   // GET /profiles/:id
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.profilesService.findOne(id);
   }
 
   // POST /profiles
+  // @Post()
+  // create(@Body() createProfileDto: CreateProfileDto) {
+  //   return {
+  //     name: createProfileDto.name,
+  //     description: createProfileDto.description,
+  //   };
+  // }
   @Post()
   create(@Body() createProfileDto: CreateProfileDto) {
-    return {
-      name: createProfileDto.name,
-      description: createProfileDto.description,
-    };
+    return this.profilesService.create(createProfileDto);
   }
 
   // PUT /profiles/:id
+  // @Put(':id')
+  // update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto) {
+  //   return {
+  //     id,
+  //     ...updateProfileDto,
+  //   };
+  // }
   @Put(':id')
   update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto) {
-    return {
-      id,
-      ...updateProfileDto,
-    };
+    return this.profilesService.update(id, updateProfileDto);
   }
 
   // DELETE /profiles/:id
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
-    return { id };
+    return this.profilesService.remove(id);
   }
 }
---
+++ src/profiles/profiles.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import type { CreateProfileDto } from './dto/create-profile.dto';
+import type { UpdateProfileDto } from './dto/update-profile.dto';
+
+@Injectable()
+export class ProfilesService {
+  private profiles = [
+    {
+      id: randomUUID(),
+      name: 'Brianna Watts',
+      description: `Looking for someone to merge with my heart. I’m a full-stack romantic who refactors my feelings until they pass all tests. Bonus points if you can debug my issues while we pair program over coffee. Let’s commit to something beautiful together.`,
+    },
+    {
+      id: randomUUID(),
+      name: 'Jasper Quinn',
+      description: `Seeking a partner in crime to compile my heart. Must be comfortable with the terminal because I only speak fluent bash. Swipe right if you can appreciate a good kernel panic every now and then.`,
+    },
+    {
+      id: randomUUID(),
+      name: 'Leo Park',
+      description: `You think you know VIM? Try Neovim. I'll make your modal dreams come true. Want to escape the matrix and explore the perfect keyboard shortcut for love?`,
+    },
+  ];
+
+  findAll() {
+    return this.profiles;
+  }
+
+  findOne(id: string) {
+    return this.profiles.find((profile) => profile.id === id);
+  }
+
+  create(createProfileDto: CreateProfileDto) {
+    const createdProfile = {
+      id: randomUUID(),
+      ...createProfileDto,
+    };
+
+    this.profiles.push(createdProfile);
+    return createdProfile;
+  }
+
+  update(id: string, updateProfileDto: UpdateProfileDto) {
+    const matchingProfile = this.profiles.find(
+      (existingProfile) => existingProfile.id === id,
+    );
+
+    if (!matchingProfile) {
+      return {};
+    }
+
+    matchingProfile.name = updateProfileDto.name;
+    matchingProfile.description = updateProfileDto.description;
+
+    return matchingProfile;
+  }
+
+  remove(id: string): void {
+    const matchingProfileIndex = this.profiles.findIndex(
+      (profile) => profile.id === id,
+    );
+
+    if (matchingProfileIndex > -1) {
+      this.profiles.splice(matchingProfileIndex, 1);
+    }
+  }
+}
